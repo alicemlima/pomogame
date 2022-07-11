df1829cbf089ccf097a97b05c7fa0b9ac19892f8
Alice <alicemariaferreiradelima@gmail.com>
2022-07-10 22:21:46 -0300

    Primeiro commit com tudo funcionando (dependencias)

---
+++ src/main/java/com/alicemlima/pomogame/PomogameApplication.java
@@ -0,0 +1,13 @@
+package com.alicemlima.pomogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomogameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alicemlima/pomogame/PomogameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alicemlima.pomogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomogameApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
